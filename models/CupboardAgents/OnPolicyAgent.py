ed87ceaa6e15b0d5078eac29faf93c78907adc65
Valay Dave <valaygaurang@gmail.com>
2020-04-10 19:32:12 -0700

    Adding design changes to nn.

--- models/CupboardAgents/OnPolicyAgent.py
+++ models/CupboardAgents/OnPolicyAgent.py
@@ -18,11 +18,11 @@ import logger
 
 class FullyConnectedPolicyEstimator(nn.Module):
     
-    def __init__(self,num_states,num_actions):
+    def __init__(self,num_states,num_actions,hidden_dims=200):
         super(FullyConnectedPolicyEstimator, self).__init__()
-        self.fc1 = nn.Linear(num_states, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, num_actions)
+        self.fc1 = nn.Linear(num_states, hidden_dims)
+        self.fc2 = nn.Linear(hidden_dims, hidden_dims)
+        self.fc3 = nn.Linear(hidden_dims, num_actions)
 
     # x is input to the network.
     def forward(self, x):
@@ -32,13 +32,55 @@ class FullyConnectedPolicyEstimator(nn.Module):
         return x
 
 
-class GripperOpenEstimator(nn.Module):
-    def __init__(self):
-        super(GripperOpenEstimator,self).__init__()
-        self.wrist_camera_rgb_convolution_NN = None
-        self.end_effector_pose_NN = None
-        self.fc3 = nn.Linear(200, 1)
+class ConvolutionalPolicyEstimator(nn.Module):
 
+    def __init__(self,num_actions,hidden_dims=50):
+        super(ConvolutionalPolicyEstimator, self).__init__()
+        # Image will be 128 * 128
+        self.cnn_layers = nn.Sequential(
+            # Defining a 2D convolution layer
+            nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # Defining another 2D convolution layer
+            nn.Conv2d(4, 4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.fc1 = nn.Linear(4*32*32, hidden_dims) # Input Dims Related to Op Dims of cnn_layer
+        self.fc2 = nn.Linear(hidden_dims, hidden_dims)
+        self.fc3 = nn.Linear(hidden_dims, num_actions)
+
+    # x is input to the network.
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class GripperOpenEstimator(nn.Module):
+    def __init__(self,image_impact_dims=2,ee_impact_dims=4,hidden_dims=30):
+        super(GripperOpenEstimator,self).__init__()
+        # Wrist Camera NN
+        self.wrist_camera_rgb_convolution_NN = ConvolutionalPolicyEstimator(image_impact_dims) # Hypothesis : Output of this network will 
+        # EE Pose NN
+        self.end_effector_pose_NN = FullyConnectedPolicyEstimator(7,ee_impact_dims)
+
+        self.fc1 = nn.Linear(ee_impact_dims+image_impact_dims,hidden_dims)
+        self.fc3 = nn.Linear(hidden_dims, 1)
+
+    def forward(self,EE_Pose,wrist_rgb):
+        wrist_op = self.wrist_camera_rgb_convolution_NN(wrist_rgb)
+        ee_op = self.wrist_camera_rgb_convolution_NN(EE_Pose)
+        stacked_tensor = torch.cat((wrist_op,ee_op),1)
+        x = F.relu(self.fc1(stacked_tensor))
+        x = self.fc3(x)
+        return x
 
 
 class ActionModeEstimator(nn.Module):
@@ -47,7 +89,7 @@ class ActionModeEstimator(nn.Module):
         super(ModularPolicyEstimator, self).__init__()
         modular_policy_op_dims = 10
         # Define Modular Policy dimes
-        self.joint_pos_policy = FullyConnectedPolicyEstimator(7,modular_policy_op_dims)
+        self.joint_pos_policy = FullyConnectedPolicyEstimator(7,modular_policy_op_dims,hidden_dims=20)
         # Define connecting Fc Linear layers.
         self.fc1 = nn.Linear(20, 200)
         self.fc3 = nn.Linear(200,action_dims)
