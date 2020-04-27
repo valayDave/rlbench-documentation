9a9581399f7b717679cf7310a5e86ba1b67362f9
Valay Dave <valaygaurang@gmail.com>
2020-04-27 01:21:11 -0700

    Added Reach target RL Agent. Code is now sufficiently modular for testing.

--- SimulationEnvironment/ReachTargetRL.py
+++ SimulationEnvironment/ReachTargetRL.py
@@ -10,6 +10,7 @@ import numpy as np
 from gym import spaces
 from .Environment import SimulationEnvironment
 from .Environment import image_types,DEFAULT_ACTION_MODE,ArmActionMode
+from .RewardFunctions import RewardFunction
 
 import sys
 sys.path.append('..')
@@ -32,14 +33,16 @@ class ReplayBuffer():
 DEFAULT_ACTION_MODE = ActionMode(ArmActionMode.ABS_JOINT_VELOCITY)
 DEFAULT_TASK = ReachTarget
 
+
 class ReachTargetRLEnvironment(SimulationEnvironment):
     
-    def __init__(self, 
+    def __init__(self,
+                reward_function:RewardFunction, 
                 action_mode=DEFAULT_ACTION_MODE,\
                 task=DEFAULT_TASK,\
                 headless=True,
                 num_episodes=100, 
-                episode_length=15, 
+                episode_length=70, 
                 dataset_root=''):
 
         super(ReachTargetRLEnvironment,self).__init__(action_mode=action_mode, task=ReachTarget, headless=headless,dataset_root=dataset_root)
@@ -48,19 +51,9 @@ class ReachTargetRLEnvironment(SimulationEnvironment):
         self.episode_length = episode_length
         self.logger = logger.create_logger(__class__.__name__)
         self.logger.propagate = 0
+        self.reward_function = reward_function
 
   
-
-    def reward_function(self, state:Observation, action, rl_bench_reward):
-        """
-        reward_function : Reward function for non Sparse Rewards. 
-        Input Parameters
-        ---------- 
-        state : state observation.
-        action: action taken  by the agent in case needed for reward shaping=
-        """
-       
-        return rl_bench_reward
     #IMP
     def _get_state(self, obs:Observation,check_images=True):
         # _get_state function is present so that some alterations can be made to observations so that
@@ -89,9 +82,8 @@ class ReachTargetRLEnvironment(SimulationEnvironment):
         error = None
         state_obs = None
         obs_, reward, terminate = self.task.step(action)  # reward in original rlbench is binary for success or not
-        print("Got Step ")
         state_obs = self._get_state(obs_)
-        shaped_reward = self.reward_function(state_obs,action,reward)
+        shaped_reward = self.reward_function(self,state_obs,action,reward)
         return state_obs, shaped_reward, terminate
 
     # IMP
@@ -105,42 +97,23 @@ class ReachTargetRLEnvironment(SimulationEnvironment):
             prev_obs = obs
             agent.reset([self._get_state(obs)]) # Reset to Rescue for resetting s_t in agent on failure
             step_counter = 0 # A counter of valid_steps within episiode
-            useless_step_counter = 0 # Total steps within episode include bullshit path plans
 
             while step_counter < self.episode_length:
             # for step_counter in range(self.episode_length): # Iterate for each timestep in Episode length
                 total_steps+=1
-                useless_step_counter+=1
                 action = agent.act([prev_obs],timestep=step_counter) # Provide state s_t to agent.
                 selected_action = action
-                print("Step Counter",step_counter,agent.warmup)
-                print(action)
-                # try:
                 new_obs, reward, terminate = self.step(selected_action)
-                self.logger.info("Found Path And Got Reward : %d " % reward)
-                step_counter+=1
-                valid_steps+=1
-                # except Exception as e:
-                #     print(e)
-                #     if useless_step_counter > 20:
-                #         break
-                #     continue
-                
-                if step_counter == self.episode_length-1:
-                    terminate = True # setting termination here becuase failed trajectory. 
-                # ! In case of failure the step function will return NONE
-                # ! The agent will have to handle None state as reward and terminate are passed.
-                elif reward > 100:
-                    terminate = True # end the episode early if the objective is acheived
-
+                step_counter+=1                
                 agent.observe([new_obs],action,reward,terminate) # s_t+1,a_t,reward_t : This should also be thought out.
                 prev_obs = new_obs
                 replay_buffer.total_reward+=reward
+                if step_counter == self.episode_length-1:
+                    terminate = True # setting termination here becuase failed trajectory. 
+                
                 if valid_steps > agent.warmup:
                     agent.update()
                 if terminate:
                     self.logger.info("Terminating!!")
-                    break
-                if useless_step_counter > 20: # if the agent 
                     break
             self.logger.info("Total Reward Gain For all Epsiodes : %d"%replay_buffer.total_reward)
--- run_learning.py
+++ run_learning.py
@@ -1,7 +1,8 @@
 # import deep_learning_rl as Simulator  
 from SimulationEnvironment.ReachTargetRL import ReachTargetRLEnvironment
 from models.ReachTargetRLAgent import ReachTargetRLAgent    
+from SimulationEnvironment.RewardFunctions import EuclideanReward
 import time
-curr_env = ReachTargetRLEnvironment(dataset_root='/home/valay/Documents/robotics-data/rlbench_data',headless=True)
+curr_env = ReachTargetRLEnvironment(EuclideanReward(),dataset_root='/home/valay/Documents/robotics-data/rlbench_data',headless=False)
 agent = ReachTargetRLAgent() 
 curr_env.train_rl_agent(agent)
